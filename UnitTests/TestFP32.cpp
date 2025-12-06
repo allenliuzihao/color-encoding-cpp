e6a2fa1fa4c41fe5a9be21ef33aec616a6da6c88
Allen Liu <allenliuzihao@gmail.com>
2025-12-06 12:48:16 -0800

    refactor fp32 tests

--- UnitTests/TestFP32.cpp
+++ UnitTests/TestFP32.cpp
@@ -8,7 +8,23 @@ namespace UnitTests
 	TEST_CLASS(TestFP32)
 	{
 	public:
-		TEST_METHOD(TestFP32EncodeAndDecode)
+        TEST_METHOD(FP32Specials)
+        {
+            auto parsedFloat = Float32(0);
+            auto val = parsedFloat.value();
+            Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 0 && parsedFloat.mantissa == 0 && val == 0.0f);
+
+            parsedFloat = Float32(asfloat(0x7f800000));
+            Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 0xff && parsedFloat.mantissa == 0);
+
+            parsedFloat = Float32(asfloat(0xff800000));
+            Assert::IsTrue(parsedFloat.sign == 1 && parsedFloat.exponent == 0xff && parsedFloat.mantissa == 0);
+
+            parsedFloat = Float32(asfloat(0x7f800001));
+            Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 0xff && parsedFloat.mantissa == 1);
+        }
+
+		TEST_METHOD(FP32Normals)
 		{
             Float32 parsedFloat = Float32(0.5);
             float val = parsedFloat.value();
@@ -21,28 +37,18 @@ namespace UnitTests
             parsedFloat = Float32(0.15625);
             val = parsedFloat.value();
             Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 124 && parsedFloat.mantissa == 2097152 && val == 0.15625f);
-
-            float expectedVal = powf(2.0f, -149);
-            parsedFloat = Float32(expectedVal);
-            val = parsedFloat.value();
-            Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 0 && parsedFloat.mantissa == 1 && val == expectedVal);
-
-            parsedFloat = Float32(0);
-            val = parsedFloat.value();
-            Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 0 && parsedFloat.mantissa == 0 && val == 0.0f);
-
-            parsedFloat = Float32(asfloat(0x7f800000));
-            Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 0xff && parsedFloat.mantissa == 0);
-
-            parsedFloat = Float32(asfloat(0xff800000));
-            Assert::IsTrue(parsedFloat.sign == 1 && parsedFloat.exponent == 0xff && parsedFloat.mantissa == 0);
-
-            parsedFloat = Float32(asfloat(0x7f800001));
-            Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 0xff && parsedFloat.mantissa == 1);
-
+            
             parsedFloat = Float32(12.375);
             val = parsedFloat.value();
             Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 130 && parsedFloat.mantissa == 4587520 && val == 12.375);
 		}
+
+        TEST_METHOD(FP32Subnormals)
+        {
+            float expectedVal = powf(2.0f, -149);
+            auto parsedFloat = Float32(expectedVal);
+            auto val = parsedFloat.value();
+            Assert::IsTrue(parsedFloat.sign == 0 && parsedFloat.exponent == 0 && parsedFloat.mantissa == 1 && val == expectedVal);
+        }
 	};
 }
