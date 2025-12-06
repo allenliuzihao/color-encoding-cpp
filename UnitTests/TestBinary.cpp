f598f2422017b1b06561d6ed771dccca5a8d92ef
Allen Liu <allenliuzihao@gmail.com>
2025-12-06 14:56:44 -0800

    separate the test in to fp16 and fp32

--- UnitTests/TestBinary.cpp
+++ UnitTests/TestBinary.cpp
@@ -8,14 +8,8 @@ namespace UnitTests
     TEST_CLASS(TestBinaryConversion)
     {
     public:
-        TEST_METHOD(BinaryConversion)
+        TEST_METHOD(FP16Binary)
         {
-            auto str1 = binary_string(uint32_t(0x07804000));
-            Assert::AreEqual(str1.c_str(), "00000111100000000100000000000000");
-
-            auto str2 = binary_string(uint32_t(0x7F800000));
-            Assert::AreEqual(str2.c_str(), "01111111100000000000000000000000");
-
             auto str3 = binary_string(uint16_t(0x8000));
             Assert::AreEqual(str3.c_str(), "1000000000000000");
 
@@ -24,6 +18,18 @@ namespace UnitTests
 
             auto str5 = binary_string(uint16_t(0xfffa));
             Assert::AreEqual(str5.c_str(), "1111111111111010");
+        }
+
+        TEST_METHOD(FP32Binary)
+        {
+            auto str1 = binary_string(uint32_t(0x00000000));
+            Assert::AreEqual(str1.c_str(), "00000000000000000000000000000000");
+
+            auto str2 = binary_string(uint32_t(0x07804000));
+            Assert::AreEqual(str2.c_str(), "00000111100000000100000000000000");
+
+            auto str3 = binary_string(uint32_t(0x7F800000));
+            Assert::AreEqual(str3.c_str(), "01111111100000000000000000000000");
         }
     };
 }
