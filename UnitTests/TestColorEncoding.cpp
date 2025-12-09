3d4e5164c329cc94ac4995aba733da6d31010824
Allen Liu <allenliuzihao@gmail.com>
2025-12-08 21:04:08 -0800

    add color encoding test

---
+++ UnitTests/TestColorEncoding.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+    TEST_CLASS(TestColorEncoding)
+    {
+    public:
+        TEST_METHOD(TestR16G16B16A16)
+        {
+            
+        }
+
+        TEST_METHOD(TestR11G11B10)
+        {
+            
+        }
+
+        TEST_METHOD(TestR10G10B10A2)
+        {
+
+        }
+
+        TEST_METHOD(TestR9G9B9E5)
+        {
+
+        }
+    };
+}
