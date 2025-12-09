fea5f547d0510bed0546f8098bc107fc4d6412f5
Allen Liu <allenliuzihao@gmail.com>
2025-12-08 21:02:06 -0800

    get it to compile again

--- UnitTests/TestFloatTypes.cpp
+++ UnitTests/TestFloatTypes.cpp
@@ -10,16 +10,6 @@ namespace UnitTests
     public:
         TEST_METHOD(TestFloat3)
         {
-            float3<float> vec = { 0.5f, 1.5f, -0.5f };
-            Assert::AreEqual(0.5f, vec.x());
-            Assert::AreEqual(1.5f, vec.y());
-            Assert::AreEqual(-0.5f, vec.z());
-
-            float3<double> vec1 = { -2.0, 0.0, 3.5 };
-            Assert::AreEqual(-2.0, vec1.x());
-            Assert::AreEqual(0.0, vec1.y());
-            Assert::AreEqual(3.5, vec1.z());
-
             float3<Float32> vec2 = { 2.5f, -1.0f, 0.0f };
             Assert::AreEqual(2.5f, vec2.x().value());
             Assert::AreEqual(-1.0f, vec2.y().value());
@@ -43,18 +33,6 @@ namespace UnitTests
 
         TEST_METHOD(TestFloat4)
         {
-            float4<float> vec = { 0.5f, 1.5f, -0.5f, 2.0f };
-            Assert::AreEqual(0.5f, vec.x());
-            Assert::AreEqual(1.5f, vec.y());
-            Assert::AreEqual(-0.5f, vec.z());
-            Assert::AreEqual(2.0f, vec.w());
-
-            float4<double> vec1 = { -2.0, 0.0, 3.5, 4.5 };
-            Assert::AreEqual(-2.0, vec1.x());
-            Assert::AreEqual(0.0, vec1.y());
-            Assert::AreEqual(3.5, vec1.z());
-            Assert::AreEqual(4.5, vec1.w());
-
             float4<Float32> vec2 = { 2.5f, -1.0f, 0.0f, 3.0f };
             Assert::AreEqual(2.5f, vec2.x().value());
             Assert::AreEqual(-1.0f, vec2.y().value());
